986aea062093719a4d7f7163d73609804ebb12f0
Patryk Kacperski <p.kacperski@fivedottwelve.com>
2017-09-06 10:22:52 +0200

    Changed way json data is unboxed

---
+++ WeatherApp/ForecastUnboxer.swift
@@ -0,0 +1,90 @@
+//
+//  ForecastUnboxer.swift
+//  WeatherApp
+//
+//  Created by patryk on 06.09.2017.
+//  Copyright © 2017 patryk. All rights reserved.
+//
+
+import UIKit
+import CoreData
+import Unbox
+
+let weatherRecordsKey = "list"
+let locationKey = "city"
+let dateKey = "dt_txt"
+let temperatureKeyPath = "main.temp"
+let minTemperatureKeyPath = "main.temp_min"
+let maxTemperatureKeyPath = "main.temp_max"
+let pressureKeyPath = "main.pressure"
+let humidityKeyPath = "main.humidity"
+let conditionsKeyPath = "weather.0.main"
+let conditionsDescriptionKeyPath = "weather.0.description"
+let iconIdKeyPath = "weather.0.icon"
+let cloudinessKeyPath = "clouds.all"
+let windSpeedKeyPath = "wind.speed"
+let countryKey = "country"
+let cityKey = "name"
+let cityIdKey = "id"
+
+class ForecastUnboxer: NSObject {
+
+    static let sharedInstance = ForecastUnboxer()
+    
+    func unbox(dictionary: Dictionary<String, Any>, managedContext: NSManagedObjectContext) throws {
+        
+        let weatherRecordsMO = try self.unboxWeatherRecords(dictionary: dictionary, managedContext: managedContext)
+        
+        let locationMO: LocationMO = try self.unboxLocation(dictionary: dictionary, managedContext: managedContext)
+        
+        let forecastMO: ForecastMO = try Unboxer.performCustomUnboxing(dictionary: dictionary, closure: {unboxer in
+            
+            let forecastMO: ForecastMO = ForecastMO(context: managedContext)
+            
+            return forecastMO
+        })
+        
+        for weatherRecord in weatherRecordsMO {
+            forecastMO.addToWeatherRecords(weatherRecord)
+        }
+        forecastMO.location = locationMO
+    }
+    
+    private func unboxWeatherRecords(dictionary: Dictionary<String, Any>, managedContext: NSManagedObjectContext) throws -> [WeatherRecordMO] {
+        var weatherRecordsMO = [WeatherRecordMO]()
+        for weatherRecord in (dictionary["list"] as! Array<Any>) {
+            let weatherRecordMO: WeatherRecordMO = try Unboxer.performCustomUnboxing(dictionary: weatherRecord as! UnboxableDictionary, closure: {unboxer in
+                
+                let weatherRecordMO: WeatherRecordMO = WeatherRecordMO(context: managedContext)
+                weatherRecordMO.temperature = try unboxer.unbox(keyPath: temperatureKeyPath)
+                weatherRecordMO.minTemperature = try unboxer.unbox(keyPath: minTemperatureKeyPath)
+                weatherRecordMO.maxTemperature = try unboxer.unbox(keyPath: maxTemperatureKeyPath)
+                weatherRecordMO.pressure = try unboxer.unbox(keyPath: pressureKeyPath)
+                weatherRecordMO.humidity = try unboxer.unbox(keyPath: humidityKeyPath)
+                weatherRecordMO.conditions = unboxer.unbox(keyPath: conditionsKeyPath)
+                weatherRecordMO.conditionsDescription = unboxer.unbox(keyPath: conditionsDescriptionKeyPath)
+                weatherRecordMO.iconId = unboxer.unbox(keyPath: iconIdKeyPath)
+                weatherRecordMO.cloudiness = try unboxer.unbox(keyPath: cloudinessKeyPath)
+                weatherRecordMO.windSpeed = try unboxer.unbox(keyPath: windSpeedKeyPath)
+                weatherRecordMO.date = unboxer.unbox(key: dateKey)
+                
+                return weatherRecordMO
+            })
+            weatherRecordsMO.append(weatherRecordMO)
+        }
+        return weatherRecordsMO
+    }
+    
+    private func unboxLocation(dictionary: Dictionary<String, Any>, managedContext: NSManagedObjectContext) throws -> LocationMO {
+        let locationMO: LocationMO = try Unboxer.performCustomUnboxing(dictionary: dictionary["city"] as! UnboxableDictionary, closure: {unboxer in
+            
+            let locationMO: LocationMO = LocationMO(context: managedContext)
+            locationMO.country = unboxer.unbox(key: countryKey)
+            locationMO.city = unboxer.unbox(key: cityKey)
+            locationMO.cityId = unboxer.unbox(key: cityIdKey)
+            
+            return locationMO
+        })
+        return locationMO
+    }
+}
--- WeatherApp/ViewController.swift
+++ WeatherApp/ViewController.swift
@@ -13,15 +13,18 @@ import CoreData
 class ViewController: UIViewController {
 
     var appDelegate: AppDelegate?
+    var managedContext: NSManagedObjectContext?
     
     let lodzLocationId : String = "3093133"
     
     var handler: HttpHandler?
-    var forecast: Forecast?
+//    var forecast: Forecast?
+    var forecastMO: ForecastMO?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.appDelegate = UIApplication.shared.delegate as? AppDelegate
+        self.managedContext = appDelegate!.persistentContainer.viewContext
         self.handler = HttpClient(baseURL: "http://api.openweathermap.org/data/")
         let requestById = WeatherByIdRequest(locationId: lodzLocationId)
         let requestByLatAndLon = WeatherByLatitudeAndLongitudeRequest(latitude: "35", longitude: "139")
@@ -30,11 +33,10 @@ class ViewController: UIViewController {
             guard let result = result else {
                 return
             }
-//            print(result)
             do {
                 try self.unboxData(result: result)
             } catch {
-                print(error)
+                print("Error occurred while unboxing: \(error)")
             }
             
         })
@@ -43,61 +45,35 @@ class ViewController: UIViewController {
     }
 
     func unboxData(result: Dictionary<String, Any>) throws {
-        self.forecast = try unbox(dictionary: result)
         
-        let managedContext = appDelegate!.persistentContainer.viewContext
-        let weatherRecordMO = WeatherRecordMO(context: managedContext)
-        
-        print(self.forecast ?? "error")
-        
-        self.saveData(forecast: forecast)
-    }
-    
-    func saveData(forecast: Forecast?) {
-//        guard self.appDelegate = UIApplication.shared.delegate as! AppDelegate else {
-//            return
-//        }
-        
-        guard let forecast = forecast else {
+        guard let managedContext = self.managedContext else {
             return
         }
         
-        let managedContext = appDelegate!.persistentContainer.viewContext
+        try ForecastUnboxer.sharedInstance.unbox(dictionary: result, managedContext: managedContext)
         
-        let weatherRecordMO = WeatherRecordMO(context: managedContext)
-        weatherRecordMO.temperature = forecast.weatherRecords[0].temperature
-        weatherRecordMO.minTemperature = forecast.weatherRecords[0].minTemperature
-        weatherRecordMO.maxTemperature = forecast.weatherRecords[0].maxTemperature
-        weatherRecordMO.cloudiness = Int32(forecast.weatherRecords[0].cloudiness)
-        weatherRecordMO.conditions = forecast.weatherRecords[0].conditions
-        weatherRecordMO.conditionsDescription = forecast.weatherRecords[0].conditionsDescription
-        weatherRecordMO.date = forecast.weatherRecords[0].date
-        weatherRecordMO.humidity = forecast.weatherRecords[0].humidity
-        weatherRecordMO.iconId = forecast.weatherRecords[0].iconId
-        weatherRecordMO.pressure = forecast.weatherRecords[0].pressure
-        weatherRecordMO.windSpeed = forecast.weatherRecords[0].windSpeed
-        
-        let locationMO = LocationMO(context: managedContext)
-        locationMO.city = forecast.location.city
-        locationMO.cityId = forecast.location.cityId
-        locationMO.country = forecast.location.country
-        
-        let forecastMO = ForecastMO(context: managedContext)
-        forecastMO.addToWeatherRecords(weatherRecordMO)
-        forecastMO.location = locationMO
+        self.saveData()
+    }
+    
+    func saveData() {
+        guard let managedContext = self.managedContext else {
+            return
+        }
         
         do {
             try managedContext.save()
         } catch {
             print("could not save \(error)")
         }
-        
     }
     
     @IBAction func fetch(_ sender: Any) {
-        let managedContext = appDelegate!.persistentContainer.viewContext
         
         let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: "Forecast")
+        
+        guard let managedContext = self.managedContext else {
+            return
+        }
         
         do {
             let forecasts: [ForecastMO] = try managedContext.fetch(fetchRequest) as! [ForecastMO]
